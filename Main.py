49231e6744ed370f735bac72568a296b7686a9fe
Timo Vijn <timovijn@gmail.com>
2020-05-05 15:16:55 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,12 @@
 import cv2
-import numpy as np
 
-cap = cv2.VideoCapture('Data/terrace1-c0.avi')
-
-if cap.isOpened() == False:
-    print("Error opening video file")
-
-while cap.isOpened():
-    ret, frame = cap.read()
-    if ret == True:
-        cv2.imshow('Frame', frame)
-    elif cv2.waitKey(1) == ord('q'):
-        break
-    else:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+cap = cv2.VideoCapture("terrace1-c0.avi")
+ret, frame = cap.read()
+while(1):
+   ret, frame = cap.read()
+   cv2.imshow('frame',frame)
+   if cv2.waitKey(1) & 0xFF == ord('q') or ret==False :
+       cap.release()
+       cv2.destroyAllWindows()
+       break
+   cv2.imshow('frame',frame)
