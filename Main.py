34b81d40b832d43c17f98bf28b0909aafff89e25
Timo Vijn <timovijn@gmail.com>
2020-05-05 23:27:31 +0200

    Update

--- Main.py
+++ Main.py
@@ -30,9 +30,11 @@ thresh = 0.5
 
 wgt_path = "./yolov4.weights"
 cfg_path = "./yolov4.cfg"
+labelsPath = "./coco.names"
 
 net = cv2.dnn.readNetFromDarknet(cfg_path, wgt_path)
 ln = net.getLayerNames()
+labels = open(labelsPath).read().strip().split("\n")
 
 blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416), swapRB=True, crop=False)
 
@@ -46,17 +48,19 @@ confidences = []
 classIDs = []
 
 for output in layerOutputs:
-    scores = detection[5:]
-    classID = np.argmax(scores)
-    confidence = scores[classID]
-    if LABELS[classID] == "person":
-        if confidence > confid:
-            box = detection[0:4] * np.array([img_w, img_h, img_w, img_h])
-            (centerX, centerY, width, height) = box.astype("int")
-            x = int(centerX - (width / 2))
-            y = int(centerY - (height / 2))
-            boxes.append([x, y, int(width), int(height)])
-            confidences.append(float(confidence))
-            classIDs.append(classID)
+    for detection in output:
+        scores = detection[5:]
+        classID = np.argmax(scores)
+        confidence = scores[classID]
+        if labels[classID] == "person":
+            if confidence > confid:
+                box = detection[0:4] * np.array([img_w, img_h, img_w, img_h])
+                (centerX, centerY, width, height) = box.astype("int")
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
 
 print(box)
+print('Done_2')
