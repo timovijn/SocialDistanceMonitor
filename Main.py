abe8ffd8377c83a09d28f56dae2c1e2a78c114df
Timo Vijn <timovijn@gmail.com>
2020-05-06 15:15:25 +0200

    Update

--- Main.py
+++ Main.py
@@ -5,100 +5,138 @@ import numpy as np
 # time.sleep(5)
 
 vid_path = "./terrace1-c0.avi"
+# vid_path = "./video.mp4"
+
+# vid = vid_path
+
+##########################
+# from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+# ffmpeg_extract_subclip(vid_path, 10, 20, targetname="clip.mp4")
+# clip_path = "clip.mp4"
+##########################
+
+clip_start = 800
+clip_end = 1000
+
+frame_count = 0
+
+(frame_h, frame_w) = (None, None)
+
 vid_cap = cv2.VideoCapture(vid_path)
 
 (success, frame) = vid_cap.read()
 
-(frame_h, frame_w) = (None, None)
+while(True):
 
-count = 0
-while count < 500:
-    (success, frame) = vid_cap.read()
-    (frame_h, frame_w) = frame.shape[:2]
-    count += 1
-    print(count)
-    # cv2.imshow('frame', frame)
-    # cv2.waitKey(1)
+    print('frame_count=',frame_count)
 
-print('CP1')
+    if frame_count >= clip_start and frame_count <= clip_end:
 
-cv2.imshow('Frame', frame)
-cv2.waitKey(2000)
+        (success, frame) = vid_cap.read()
+        (frame_h, frame_w) = frame.shape[:2]
 
-confid = 0.5
-thresh = 0.5
+        # cv2.imshow('frame', frame)
+        # cv2.waitKey(1)
 
-wgt_path = "./yolov3.weights"
-cfg_path = "./yolov3.cfg"
-labelsPath = "./coco.names"
+        print('CP1')
 
-net = cv2.dnn.readNetFromDarknet(cfg_path, wgt_path)
-ln = net.getLayerNames()
-ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-labels = open(labelsPath).read().strip().split("\n")
+        cv2.imshow('Frame', frame)
+        cv2.waitKey(1)
 
-frame_resized = cv2.resize(frame, (416, 416))
-blob = cv2.dnn.blobFromImage(frame_resized, 1 / 255.0, (416, 416), swapRB=True, crop=False)         # Scale image by dividing by 255. YoloV3 needs input size (416, 416)
-blobb = blob.reshape(blob.shape[2],blob.shape[3],3)
-cv2.imshow('Blob', blobb)
-cv2.waitKey(2000)
+        confid = 0.5
+        thresh = 0.5
 
-net.setInput(blob)
-start = time.time()
-layerOutputs = net.forward(ln)
-end = time.time()
+        wgt_path = "./yolov3.weights"
+        cfg_path = "./yolov3.cfg"
+        labelsPath = "./coco.names"
 
-boxes = []
-confidences = []
-classIDs = []
+        net = cv2.dnn.readNetFromDarknet(cfg_path, wgt_path)
+        ln = net.getLayerNames()
+        ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+        labels = open(labelsPath).read().strip().split("\n")
 
-for output in layerOutputs:
-    for detection in output:
-        scores = detection[5:]
-        classID = np.argmax(scores)
-        confidence = scores[classID]
-        if labels[classID] == "person":
-            if confidence > confid:
-                box = detection[0:4] * np.array([frame_w, frame_h, frame_w, frame_h])
-                (centerX, centerY, width, height) = box.astype("int")
-                x = int(centerX - (width / 2))
-                y = int(centerY - (height / 2))
-                boxes.append([x, y, int(width), int(height)])
-                confidences.append(float(confidence))
-                classIDs.append(classID)
-idxs = cv2.dnn.NMSBoxes(boxes, confidences, confid, thresh)
+        frame_resized = cv2.resize(frame, (416, 416))
+        blob = cv2.dnn.blobFromImage(frame_resized, 1 / 255.0, (416, 416), swapRB=True, crop=False)         # Scale image by dividing by 255. YoloV3 needs input size (416, 416)
+        blobb = blob.reshape(blob.shape[2],blob.shape[3],3)
+        cv2.imshow('Blob', blobb)
+        cv2.waitKey(1)
 
-print(idxs)
-print(confidences)
+        net.setInput(blob)
+        start = time.time()
+        layerOutputs = net.forward(ln)
+        end = time.time()
 
-print('CP2')
+        boxes = []
+        confidences = []
+        classIDs = []
 
-if len(idxs) > 0:
-    status = []
-    idf = idxs.flatten()
-    close_pair = []
-    s_close_pair = []
-    centers = []
-    co_info = []
+        for output in layerOutputs:
+            for detection in output:
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+                if labels[classID] == "person":
+                    if confidence > confid:
+                        box = detection[0:4] * np.array([frame_w, frame_h, frame_w, frame_h])
+                        (centerX, centerY, width, height) = box.astype("int")
+                        x = int(centerX - (width / 2))
+                        y = int(centerY - (height / 2))
+                        boxes.append([x, y, int(width), int(height)])
+                        confidences.append(float(confidence))
+                        classIDs.append(classID)
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, confid, thresh)
 
-    for i in idf:
+        print(idxs)
+        print(confidences)
 
-        (x[i], y[i]) = (boxes[i][0], boxes[i][1])
-        (w[i], h[i]) = (boxes[i][2], boxes[i][3])
-        center = [int(x + w / 2), int(y + h / 2)]
-        centers.append(center)
-        cv2.circle(frame, tuple(center),1,(0,0,0),1)
-        co_info.append([w, h, center])
-        
-        status.append(0)
+        print('CP2')
 
-    print(centers)
+        if len(idxs) > 0:
+            status = []
+            idf = idxs.flatten()
+            print('idf =', idf)
+            close_pair = []
+            s_close_pair = []
+            centers = []
+            co_info = []
+            X = []
+            Y = []
+            W = []
+            H = []
 
-    print(x,y,h,w)
+            for i in idf:
 
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 150), 2)
+                (x, y) = (boxes[i][0], boxes[i][1])
+                X.append(x)
+                Y.append(y)
+                (w, h) = (boxes[i][2], boxes[i][3])
+                W.append(w)
+                H.append(h)
+                center = [int(x + w / 2), int(y + h / 2)]
+                centers.append(center)
+                cv2.circle(frame, tuple(center),1,(0,0,0),1)
+                co_info.append([w, h, center])
+                
+                status.append(0)
 
-    cv2.imshow('Social distancing analyser', frame)
-    cv2.waitKey(2000)
+            print(centers)
+            print(X)
+            print(Y)
 
-print('CP3')
+            ilist = []
+            i = 0
+
+            for i in range(0,len(idf)):
+                ilist.append(i)
+                print(i)
+                cv2.rectangle(frame, (X[i], Y[i]), (X[i] + W[i], Y[i] + H[i]), (0, 0, 150), 2)
+            cv2.imshow('Social distancing analyser', frame)
+            cv2.waitKey(1)
+
+
+        print('CP3')
+
+    else:
+        (success, frame) = vid_cap.read()
+
+    frame_count += 1
