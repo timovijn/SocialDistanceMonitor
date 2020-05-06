ea06317219336c2165cbd5e77c7ccbb7cfd6d490
Timo Vijn <timovijn@gmail.com>
2020-05-06 13:59:22 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,23 +4,23 @@ import numpy as np
 
 # time.sleep(5)
 
-vid_path = "./video.mp4"
-vidcap = cv2.VideoCapture(vid_path)
+vid_path = "./terrace1-c0.avi"
+vid_cap = cv2.VideoCapture(vid_path)
 
-(success, frame) = vidcap.read()
+(success, frame) = vid_cap.read()
 
-(img_h, img_w) = (None, None)
+(frame_h, frame_w) = (None, None)
 
 count = 0
 while count < 500:
-    (success, frame) = vidcap.read()
-    (img_h, img_w) = frame.shape[:2]
+    (success, frame) = vid_cap.read()
+    (frame_h, frame_w) = frame.shape[:2]
     count += 1
     print(count)
     # cv2.imshow('frame', frame)
     # cv2.waitKey(1)
 
-print('Done')
+print('CP1')
 
 cv2.imshow('Frame', frame)
 cv2.waitKey(2000)
@@ -35,24 +35,18 @@ labelsPath = "./coco.names"
 net = cv2.dnn.readNetFromDarknet(cfg_path, wgt_path)
 ln = net.getLayerNames()
 ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
-print(ln)
 labels = open(labelsPath).read().strip().split("\n")
 
 frame_resized = cv2.resize(frame, (416, 416))
 blob = cv2.dnn.blobFromImage(frame_resized, 1 / 255.0, (416, 416), swapRB=True, crop=False)         # Scale image by dividing by 255. YoloV3 needs input size (416, 416)
-print(blob)
-print(blob.shape)
 blobb = blob.reshape(blob.shape[2],blob.shape[3],3)
-print(blobb.shape)
 cv2.imshow('Blob', blobb)
-cv2.waitKey(20000)
+cv2.waitKey(2000)
 
 net.setInput(blob)
 start = time.time()
 layerOutputs = net.forward(ln)
 end = time.time()
-
-print(layerOutputs)
 
 boxes = []
 confidences = []
@@ -65,12 +59,46 @@ for output in layerOutputs:
         confidence = scores[classID]
         if labels[classID] == "person":
             if confidence > confid:
-                box = detection[0:4] * np.array([img_w, img_h, img_w, img_h])
+                box = detection[0:4] * np.array([frame_w, frame_h, frame_w, frame_h])
                 (centerX, centerY, width, height) = box.astype("int")
                 x = int(centerX - (width / 2))
                 y = int(centerY - (height / 2))
                 boxes.append([x, y, int(width), int(height)])
                 confidences.append(float(confidence))
                 classIDs.append(classID)
+idxs = cv2.dnn.NMSBoxes(boxes, confidences, confid, thresh)
 
-print('Done_2')
+print(idxs)
+print(confidences)
+
+print('CP2')
+
+if len(idxs) > 0:
+    status = []
+    idf = idxs.flatten()
+    close_pair = []
+    s_close_pair = []
+    center = []
+    co_info = []
+
+    for i in idf:
+
+        (x, y) = (boxes[i][0], boxes[i][1])
+        (w, h) = (boxes[i][2], boxes[i][3])
+        cen = [int(x + w / 2), int(y + h / 2)]
+        center.append(cen)
+        cv2.circle(frame, tuple(cen),1,(0,0,0),1)
+        co_info.append([w, h, cen])
+        
+        status.append(0)
+
+print(center)
+
+print(x,y,h,w)
+
+cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 150), 2)
+
+cv2.imshow('Social distancing analyser', frame)
+cv2.waitKey(2000)
+
+print('CP3')
