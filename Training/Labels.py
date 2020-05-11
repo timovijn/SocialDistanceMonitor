c4148f774128a91d17e8c30965f53508f1e7c46c
Timo Vijn <timovijn@gmail.com>
2020-05-11 17:20:05 +0200

    Update

--- Training/Labels.py
+++ Training/Labels.py
@@ -8,24 +8,31 @@ import time
 import numpy as np
 import imutils
 
-directory = "./Training/Images/"
+directory = "./Training/Images/Test/"
   
 for filename in os.listdir(directory):
-    img_path = "./Training/Images/{}".format(filename)
+    img_path = "./Training/Images/Test/{}".format(filename)
     img = cv2.imread(img_path)
     (img_h, img_w) = img.shape[:2]
 
     filename = filename.replace(".png","")
     print(filename)
     
-    file = open('./Training/Labels/{}.txt'.format(filename),'w')
+    label_file = open('./Training/Labels/Test/{}.txt'.format(filename),'w')
 
-    file.write('1 \n')
-    file.write('0 0 ')
-    file.write("{} ".format(img_h))
-    file.write("{}".format(img_w))
+    label_file.write('0 ')
+    label_file.write('0.5 0.5 ')
+    label_file.write('1 1')
 
-    file.close()
+    # file.write("{} ".format(img_h))
+    # file.write("{}".format(img_w))
+
+    label_file.close()
+
+    list_file = open('./Training/Labels/Test/Test.txt', 'a')
+    
+    list_file.write('{} \n'.format(img_path))
+    
+    list_file.close()
+
     continue
-
-
